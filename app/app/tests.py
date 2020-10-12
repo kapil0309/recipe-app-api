81bbca73ed283fbeec47568837eaeb954c0d174f
kapil0309 <kapil.yadav3@tcs.com>
2020-10-12 18:11:12 +0530

    test

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,4 +6,4 @@ class CalcTests(TestCase):
 
     def test_add_numbers(self):
         """Tets that two  numbers are added together"""
-        self.assertEqual(add(3,8), 11)
+        self.assertEqual(add(3, 8), 11)
