5a23b1c9949b93d4a6297b8a68ac99c8931cb2b4
kapil0309 <kapil.yadav3@tcs.com>
2020-10-14 11:15:23 +0530

    r

--- app/core/models.py
+++ app/core/models.py
@@ -17,7 +17,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=true)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BoolenaField(default=false)
 
     objects = UserManager()
