8da29111dcb41276869ba0f72b261900b3470e3c
kapil0309 <kapil.yadav3@tcs.com>
2020-10-14 10:48:57 +0530

    q

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionMixin
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """creates and saves the new user"""
+
+        user = self.model(email=email, ^^extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionMixin):
+    """Custom user model that supports using email instead of username"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=true)
+    is_staff = models.BoolenaField(default=false)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
